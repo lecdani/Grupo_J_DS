1a7e87416e1969274ed37fc213e480f4b9a01a8f
Andrés Uribe <130877641+aeuribe@users.noreply.github.com>
2023-12-27 00:42:11 -0400

    Se crean los Dao de Querella y P_Q

---
+++ src/persistence/dao/src/main/java/com/ucab/cmcapp/persistence/dao/QuerellaDao.java
@@ -0,0 +1,30 @@
+package com.ucab.cmcapp.persistence.dao;
+
+import com.ucab.cmcapp.common.EntityFactory;
+import com.ucab.cmcapp.common.entities.*;
+import com.ucab.cmcapp.common.exceptions.CupraException;
+import com.ucab.cmcapp.persistence.DBHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class QuerellaDao extends BaseDao<Querella>{
+    private static Logger _logger = LoggerFactory.getLogger( QuerellaDao.class );
+    private EntityManager _em;
+    private CriteriaBuilder _builder;
+
+    public QuerellaDao(){
+        super();
+    }
+
+    public QuerellaDao(DBHandler handler){
+        super(handler);
+    }
+
+
+}
